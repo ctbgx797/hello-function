4d9b3f3ad195912a203a0b0707ee3225f6b33df3
Yoshiki <villager2525@gmail.com>
2019-04-17 16:58:25 +0900

    3テーブルの割り勘を計算をコピペで実現

---
+++ warikan.py
@@ -0,0 +1,17 @@
+# Table2: 2000円で3人
+amount = 1500
+number_of_people = 3
+
+print(f"一人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+
+# Table3: 3647円で4人
+amount = 2000
+number_of_people = 3
+
+print(f"一人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+
+# Table1: 1500円で3人
+amount = 3647
+number_of_people = 4
+
+print(f"一人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
