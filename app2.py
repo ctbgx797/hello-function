881c9e842de0b297b1b1fcbd956d096711594070
Yoshiki <villager2525@gmail.com>
2019-04-17 18:53:24 +0900

    リファクタリング: importで出力
    これええやん便利
    
    Option+Enter inportで関数さくせい
    Ctrl+T renameでInport先の関数も置換できる

---
+++ app2.py
@@ -0,0 +1,4 @@
+# 割り勘計算した~い 7000円で6人
+from warikan2 import caliculate_warikan
+
+print(caliculate_warikan(amount=1500, number_of_people=3))
--- warikan2.py
+++ warikan2.py
@@ -1,4 +1,4 @@
-def warikan(amount, number_of_people):
+def caliculate_warikan(amount, number_of_people):
     quotient = amount // number_of_people
     remainder = amount % number_of_people
 
@@ -6,10 +6,10 @@ def warikan(amount, number_of_people):
 
 
 # Table1: 1500円で3人
-print(warikan(amount=1500, number_of_people=3))
+# print(warikan(amount=1500, number_of_people=3))
 # Table2: 2000円で3人
-print(warikan(amount=2000, number_of_people=3))
-# Table3: 3647円で4人
-print(warikan(amount=3647, number_of_people=4))
-# Table5: 5000円で6人
-print(warikan(amount=5000, number_of_people=6))
+# print(warikan(amount=2000, number_of_people=3))
+# # Table3: 3647円で4人
+# print(warikan(amount=3647, number_of_people=4))
+# # Table5: 5000円で6人
+# print(warikan(amount=5000, number_of_people=6))
